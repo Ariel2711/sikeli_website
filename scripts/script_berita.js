9aa8176e539112fcc184c65388f04bbdac131613
Althaf Rizal <ilikemud7@gmail.com>
2024-05-28 11:31:07 +0700

    Update: resources(gambar), js berita, forum, home, html detail_berita

--- scripts/script_berita.js
+++ scripts/script_berita.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
             "title": "Neta Buatan Bekasi Belum Lirik Pasar Ekspor",
             "description": "Pabrikan asal China, Neta, tak main-main terjun ke pasar Indonesia. Buktinya salah satu model yang dijual saat ini yaitu Neta V-II sudah diproduksi di Tanah Air.",
             "link": "detail_berita.html",
-            "image": "../images/slider1.jpg",
+            "image": "../images/Neta-Berita.jpg",
             "category": {
                 "text": "Mobil Listrik",
                 "color": "bg-primary",
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
             "title": "ECGO Atasi Kendala Ojol Pakai Motor Listrik, Bisa Jalan 200 Km Sehari",
             "description": "Pengendara ojek online atau ojol saat ini sudah mulai menggunakan sepeda motor listrik. Cuma sebagian besar, memang masih mengandalkan motor bensin konc=vensional.",
             "link": "detail_berita.html",
-            "image": "../images/slider2.jpg",
+            "image": "../images/ECGO-Berita.jpg",
             "category": {
                 "text": "Motor Listrik",
                 "color": "bg-success",
@@ -28,13 +28,13 @@ document.addEventListener("DOMContentLoaded", function () {
             "title": "Airlangga Hartarto Bocorkan Status Pabrik Baterai Mobil Listrik Hyundai di RI.",
             "description": "Menteri Koordinator Bidang Perekonomian, Airlangga Hartarto membocorkan status pabrik baterai Hyundai di Indonesia. Tempat pembuatan penyimpan energi untuk mobil listrik itu akan beroperasi dalam waktu dekat.",
             "link": "detail_berita.html",
-            "image": "../images/slider3.jpg",
+            "image": "../images/Pabrik-Hyundai-Berita.jpg",
             "category": {
                 "text": "Berita Umum",
                 "color": "bg-danger",
                 "textColor": "text-danger"
             },
-            "date": "20 April 2024"
+            "date": "2 Januari 2024"
         }
     ];
 
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", function () {
             "title": "Neta Buatan Bekasi Belum Lirik Pasar Ekspor",
             "description": "Pabrikan asal China, Neta, tak main-main terjun ke pasar Indonesia. Buktinya salah satu model yang dijual saat ini yaitu Neta V-II sudah diproduksi di Tanah Air.",
             "link": "detail_berita.html",
-            "image": "../images/slider1.jpg",
+            "image": "../images/Neta-Berita.jpg",
             "category": {
                 "text": "Mobil Listrik",
                 "color": "bg-primary",
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", function () {
             "title": "ECGO Atasi Kendala Ojol Pakai Motor Listrik, Bisa Jalan 200 Km Sehari",
             "description": "Pengendara ojek online atau ojol saat ini sudah mulai menggunakan sepeda motor listrik. Cuma sebagian besar, memang masih mengandalkan motor bensin konc=vensional.",
             "link": "detail_berita.html",
-            "image": "../images/slider2.jpg",
+            "image": "../images/ECGO-Berita.jpg",
             "category": {
                 "text": "Motor Listrik",
                 "color": "bg-success",
@@ -93,78 +93,78 @@ document.addEventListener("DOMContentLoaded", function () {
             "title": "Airlangga Hartarto Bocorkan Status Pabrik Baterai Mobil Listrik Hyundai di RI.",
             "description": "Menteri Koordinator Bidang Perekonomian, Airlangga Hartarto membocorkan status pabrik baterai Hyundai di Indonesia. Tempat pembuatan penyimpan energi untuk mobil listrik itu akan beroperasi dalam waktu dekat.",
             "link": "detail_berita.html",
-            "image": "../images/slider3.jpg",
+            "image": "../images/Pabrik-Hyundai-Berita.jpg",
             "category": {
                 "text": "Berita Umum",
                 "color": "bg-danger",
                 "textColor": "text-danger"
             },
-            "date": "20 April 2024",
+            "date": "2 Januari 2024",
             "class": "Terpopuler Berita-Umum"
         },
         {
             "title": "Yuanhang Auto resmi luncurkan mobil listrik H9.",
             "description": "Jenama otomotif yang fokus dalam pengembangan kendaraan elektrik, Yuanhang Auto resmi menghadirkan kendaraan jenis Sport Utility Vehicle (SUV) listrik murni H9 dengan tiga pilihan jarak jelajah 650 km, 840 km, dan 950 km.",
             "link": "detail_berita.html",
-            "image": "../images/slider1.jpg",
+            "image": "../images/Yuanhang-H9-Berita.jpg",
             "category": {
                 "text": "Mobil Listrik",
                 "color": "bg-primary",
                 "textColor": "text-primary"
             },
-            "date": "15 April 2024",
+            "date": "15 Desember 2023",
             "class": "Mobil"
         },
         {
             "title": "Makin Banyak Merek Motor Listrik Bikin Pabrik di Indonesia, Aismoli Bilang Begini.",
             "description": "Ketua Umum Asosiasi Industri Motor Listrik Indonesia (Aismoli) Budi Setiyadi mengungkapkan sudah banyak merek motor listrik yang berinvestasi membangun pabrik di Tanah Air. Bahkan, menurut dia di bulan ini sudah ada tiga pabrikan yang menggelar groundbreaking pembangunan pabriknya.",
             "link": "detail_berita.html",
-            "image": "/images/slider1.jpg",
+            "image": "/images/Motor-Banyak-Berita.jpg",
             "category": {
                 "text": "Motor Listrik",
                 "color": "bg-success",
                 "textColor": "text-success"
             },
-            "date": "14 Februari 2024",
+            "date": "14 Desember 2023",
             "class": "Motor"
         },
         {
             "title": "Jaringan Terus Diperluas, Voltron Buka 6 SPKLU Lagi di PIK.",
             "description": "PT Exelly Elektrik Indonesia (Voltron), terus memperluas jaringan Stasiun Pengisian Kendaraan Listrik Umum (SPKLU) untuk mendukung ekosistem di Indonesia. Kali ini, operator penyedia pengisian baterai swasta tersebut, menambah enam mesin di area parkir kendaraan Batavia Pantai Indah Kapuk (PIK) selama 1x24 jam, dengan fasilitas yang nyaman.",
             "link": "detail_berita.html",
-            "image": "../images/slider3.jpg",
+            "image": "../images/Voltron-SPKLU-PIK-Berita.jpeg",
             "category": {
                 "text": "Berita Umum",
                 "color": "bg-danger",
                 "textColor": "text-danger"
             },
-            "date": "20 Januari 2024",
+            "date": "20 November 2023",
             "class": "Berita-Umum"
         },
         {
             "title": "Bidik Pasar RI, Produsen Motor Listrik Sunra Hadirkan Produk Harga Ekonomis.",
             "description": "Produsen motor listrik PT Sunra Asia Pacific Hi-Tech atau Sunra Indonesia berupaya meningkatkan eksposur dan mempertegas posisinnya di Indonesia",
             "link": "detail_berita.html",
-            "image": "/images/slider1.jpg",
+            "image": "/images/Sunra-Ekonomis-Berita.jpg",
             "category": {
                 "text": "Motor Listrik",
                 "color": "bg-success",
                 "textColor": "text-success"
             },
-            "date": "27 Desember 2023",
+            "date": "27 Oktober 2023",
             "class": "Motor"
         },
         {
             "title": "Inkonsistensi Pemerintah Jadi Penyebab Turunnya Penjualan Mobil Listrik",
             "description": "Pemerintah sering mengubah kebijakan kendaraan listrik, yang hasilnya membuat sebagian besar masyarakat Indonesia menahan pembelian mobil listrik berbasis baterai, sehingga embuat laju pertumbuhan pasar otomotif nasional melambat.",
             "link": "detail_berita.html",
-            "image": "/images/mobil1.jpg",
+            "image": "/images/Turun-MobilListrik-Berita.jpg",
             "category": {
                 "text": "Berita Umum",
                 "color": "bg-danger",
                 "textColor": "text-danger"
             },
-            "date": "13 Januari 2024",
+            "date": "13 Oktober 2023",
             "class": "Berita-Umum"
 
         },
@@ -172,26 +172,26 @@ document.addEventListener("DOMContentLoaded", function () {
             "title": "Cara AHM Terapkan ESG.",
             "description": "Tak hanya menyiapkan produk sepeda motor listrik sebagai upaya netralitas karbon, PT Astra Honda Motor (AHM) bersama jaringannya juga mewujudkan komitmen dalam mendukung implementasi Environmental, Social, and Governance (ESG) yang baik.",
             "link": "detail_berita.html",
-            "image": "/images/slider1.jpg",
+            "image": "/images/AHM-ESG-Berita.jpeg",
             "category": {
                 "text": "Motor Listrik",
                 "color": "bg-success",
                 "textColor": "text-success"
             },
-            "date": "31 Desember 2023",
+            "date": "30 September 2023",
             "class": "Motor"
         },
         {
             "title": "Mobil Listrik Termurah Hyundai di Indonesia Mau Dijual Bulan Depan",
             "description": "Generasi pertama SUV kecil Kona pertama kali muncul di Korea Selatan pada 2017, mobil ini awalnya hanya ditawarkan dengan mesin bensin 2.000 cc dan 1.600 cc turbo. Versi Kona Electric muncul pada 2018.",
             "link": "detail_berita.html",
-            "image": "/images/mobil2.jpg",
+            "image": "/images/Mobil-Termurah-Hyundai-Berita.jpeg",
             "category": {
                 "text": "Mobil Listrik",
                 "color": "bg-primary",
                 "textColor": "text-primary"
             },
-            "date": "20 Maret 2024",
+            "date": "20 September 2023",
             "class": "Mobil"
 
         },
@@ -199,13 +199,13 @@ document.addEventListener("DOMContentLoaded", function () {
             "title": "Perbandingan Harga Hyundai Kona Electric CBU dan Rakitan Lokal, Turun Ratusan Juta!",
             "description": "Hyundai Kona Electric rakitan lokal bakal menjadi mobil listrik termurah Hyundai yang dijual di Indonesia. Sejatinya Kona Electric pernah mewarnai jalanan Indonesia tapi statusnya diimpor dari Korea Selatan.",
             "link": "detail_berita.html",
-            "image": "/images/slider1.jpg",
+            "image": "/images/Hyundai-Kona-Electric-Berita.jpeg",
             "category": {
                 "text": "Mobil Listrik",
                 "color": "bg-primary",
                 "textColor": "text-primary"
             },
-            "date": "2 Februari 2024",
+            "date": "2 September 2023",
             "class": "Terpopuler Mobil"
 
         },
@@ -213,13 +213,13 @@ document.addEventListener("DOMContentLoaded", function () {
             "title": "Kunker ke Korsel, Airlangga Bahas Pengembangan Mobil Listrik-Petrokimia RI",
             "description": "Menteri Koordinator Bidang Perekonomian Airlangga Hartarto melakukan kunjungan kerja ke Korea Selatan. Pihaknya membahas berbagai peluang investasi Korea Selatan di Indonesia, seperti pengembangan pabrik mobil Hyundai, pembangunan pabrik petrokimia oleh Lotte Chemical, dan komitmen pembangunan klaster industri baja berkapasitas 10 juta ton pada 2025.",
             "link": "detail_berita.html",
-            "image": "/images/mobil1.jpg",
+            "image": "/images/Airlangga-Bahas-Mobil-Listrik-Berita.jpeg",
             "category": {
                 "text": "Berita Umum",
                 "color": "bg-danger",
                 "textColor": "text-danger"
             },
-            "date": "13 April 2024",
+            "date": "13 Agustus 2023",
             "class": "Berita-Umum"
 
         }
